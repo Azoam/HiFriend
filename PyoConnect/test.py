e470f809919af2f6c397ffdcfe05c272d4b36a0e
azoam <sazouzi21@gmail.com>
2018-04-08 00:20:24 +0000

    working test data

--- PyoConnect/test.py
+++ PyoConnect/test.py
@@ -2,14 +2,16 @@ from PyoConnectLib import *
 myo = Myo(sys.argv[1] if len (sys.argv) >= 2 else None)
 
 
-def something(pose):
-    print("AAAAH"+str(pose))
+def something(quat, acc, gyro):
+    print("QUAT"+str(quat))
+    print("ACC"+str(acc))
+    print("GYRO"+str(gyro))
 
 
 
 
 
-myo.add_pose_handler(something)
+myo.add_imu_handler(something)
 myo.connect()
 try:
     while True:
