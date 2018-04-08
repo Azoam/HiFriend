f6e417b00808fbd480f7a49cc568286597858775
Sam Azouzi <sazouzi21@gmail.com>
2018-04-07 20:54:53 -0400

    testing for waving

--- PyoConnect/test.py
+++ PyoConnect/test.py
@@ -1,16 +1,34 @@
+import numpy
+import sys
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_cal_scare
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipline import Pipeline
 from PyoConnectLib import *
 myo = Myo(sys.argv[1] if len (sys.argv) >= 2 else None)
+featureVector = []
+MAX = 1500
 
 
 def something(quat, acc, gyro):
-    featureVector = []
     for x in quat:
         featureVector.append(x)
     for x in acc:
         featureVector.append(x)
     for x in gyro:
         featureVector.append(x)
-    
+    if len(featureVector) >= 2000:
+        for x in range(0,1500):
+            with open('data.csv','a') as file:
+                file.write(str(featureVector[x])+",")
+        with open('data.csv','a') as file:
+            file.write("2\n")
+        featureVector = []
 
 
 
